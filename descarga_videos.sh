a61b70eeb59003e087e27366d176153d55106ddd
lmorillas <morillas@gmail.com>
2018-10-23 13:29:59 +0200

    update emilio

--- descarga_videos.sh
+++ descarga_videos.sh
@@ -2,6 +2,7 @@ env/bin/python descarga_videos_bellas.py
 env/bin/python descarga_videos_xmf.py 
 env/bin/python descarga_videos_carpintero.py
 env/bin/python descarga_videos_musikepl.py
-
+cp videos_bellas/docs/static/*json ../visualizations/docs/static 
+cd ../visualizations
 git commit -a -m updated
 git push
---
+++ descarga_videos_emilio.py
@@ -0,0 +1,37 @@
+""" Pull All Youtube Videos from a Playlist """
+
+from apiclient.discovery import build
+from apiclient.errors import HttpError
+import json
+from descarga_videos import *
+from crea_ex_json import *
+
+RUTA = 'docs/static/'
+        
+if __name__ == '__main__':
+
+    yt = connect_youtube()
+
+    channelId = "UC5i2qJffEjieDZyGFzDzeFg"
+    username = ''
+
+    
+    playlists = get_playlists(yt, channelId)
+
+    #videos = fetch_all_youtube_videos(yt, "PLybE0q7GFyrbQSrWNgci8QJ60gQjQKLlH")
+    videos  = todos_los_videos_playlist(yt, playlists)
+    ids = [x['snippet']['resourceId']['videoId'] for x in videos]
+    
+    videosCanal = todos_los_videos_canal(yt, channelId)
+    videosCanal  = [v   for v in videosCanal if v['id']['kind'] == 'youtube#video' ]
+    noestan = [v for v in videosCanal if v['id']['videoId'] not in ids]
+    videos.extend(noestan)
+    #videos2 = todos_los_videos_2(yt, channelId)
+
+    json.dump(playlists, open('playlists_emiliovillalba_yt.json', 'w'))
+    json.dump(videos, open('videos_emiliovillalba_yt.json', 'w'))
+    crea_videos_exhibit('videos_emiliovillalba_yt.json', RUTA + 'videos_emiliovillalba.json')
+    crea_playlist_exhibit('playlists_emiliovillalba_yt.json', RUTA + 'playlists_emiliovillalba.json')
+        
+    
+
