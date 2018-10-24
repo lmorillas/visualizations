14d27c81bb96d13146eb716701849375babe9215
lmorillas <morillas@gmail.com>
2018-10-24 12:25:13 +0200

    updated

--- descarga_videos.sh
+++ descarga_videos.sh
@@ -2,7 +2,7 @@ env/bin/python descarga_videos_bellas.py
 env/bin/python descarga_videos_xmf.py 
 env/bin/python descarga_videos_carpintero.py
 env/bin/python descarga_videos_musikepl.py
-cp videos_bellas/docs/static/*json ../visualizations/docs/static 
-cd ../visualizations
+env/bin/python descarga_videos_emilio.py
+
 git commit -a -m updated
 git push
