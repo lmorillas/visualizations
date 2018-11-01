73b0b8411b14e0f90168f47eb3fac567f96c0a12
lmorillas <morillas@gmail.com>
2018-11-01 19:43:18 +0100

    update

--- crea_ex_json.py
+++ crea_ex_json.py
@@ -7,42 +7,16 @@ def crea_videos_exhibit(entrada, salida=None):
     videos = json.load(open(entrada))
     for v in videos:
         try:
-            if v['status']['privacyStatus'] == 'public' and \
-                v['snippet']['title'] != 'Deleted video':
-                d = v.get('snippet')
-                k = {}
-                k['url'] = d['resourceId']['videoId']
-                if k['url'] in itemsd:
-                    itemsd[k['url']]['playlistId'].append(d.get('playlistId'))
-                else:
-                    k['label'] = d.get('title')
-                    try:
-                        k['imagen'] = d['thumbnails']['medium']['url']
-                    except:
-                        try:
-                            k['imagen'] = d['thumbnails']['default']['url']
-                        except:
-                            print(k, 'no imagen')
-                            pass
-                    k['playlistId'] = [d.get('playlistId')]
-                    k['position'] = d.get('position')
-                    k['url'] = d['resourceId']['videoId']
-                    k['type'] = 'video'
-                    k['id'] = v.get('id')
-                    k['publishedAt'] = d.get('publishedAt')[:10]
-                    k['description'] = d.get('description')
-                    itemsd[k['url']] = k
-        except  Exception as e:
-            print ('Exception --> ', e)
-            if v['id']['kind'] == 'youtube#video':    
-                d = v.get('snippet')
-                k = {}
-                k['url'] = v['id']['videoId']
+            if v.get('status') and v['status']['privacyStatus'] != 'public' or \
+                v['snippet']['title'] == 'Deleted video':
+                continue
+            d = v.get('snippet')
+            k = {}
+            k['url'] = d['resourceId']['videoId']
+            if k['url'] in itemsd:
+                itemsd[k['url']]['playlistId'].append(d.get('playlistId'))
+            else:
                 k['label'] = d.get('title')
-                k['type'] = 'video'
-                k['id'] = v['id']['videoId']
-                k['publishedAt'] = d.get('publishedAt')[:10]
-                k['description'] = d.get('description')
                 try:
                     k['imagen'] = d['thumbnails']['medium']['url']
                 except:
@@ -51,7 +25,35 @@ def crea_videos_exhibit(entrada, salida=None):
                     except:
                         print(k, 'no imagen')
                         pass
+                k['playlistId'] = [d.get('playlistId')]
+                k['position'] = d.get('position')
+                k['url'] = d['resourceId']['videoId']
+                k['type'] = 'video'
+                k['id'] = v.get('id')
+                k['publishedAt'] = d.get('publishedAt')[:10]
+                k['description'] = d.get('description')
                 itemsd[k['url']] = k
+        except  Exception as e:
+            print ('Exception --> ', e)
+            if v.get('id') and v['id'].get('kind') != 'youtube#video':    
+                continue
+            d = v.get('snippet')
+            k = {}
+            k['url'] = v['id']['videoId']
+            k['label'] = d.get('title')
+            k['type'] = 'video'
+            k['id'] = v['id']['videoId']
+            k['publishedAt'] = d.get('publishedAt')[:10]
+            k['description'] = d.get('description')
+            try:
+                k['imagen'] = d['thumbnails']['medium']['url']
+            except:
+                try:
+                    k['imagen'] = d['thumbnails']['default']['url']
+                except:
+                    print(k, 'no imagen')
+                    pass
+            itemsd[k['url']] = k
     if salida:
         json.dump({'items': list(itemsd.values())}, open(salida, 'w'))
     else:
