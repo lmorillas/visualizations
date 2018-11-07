e0c83bed5298b551e4b0bd09353d0a5f747d0324
lmorillas <morillas@gmail.com>
2018-11-07 09:45:31 +0100

    bellonmaceiras

--- descarga_videos.sh
+++ descarga_videos.sh
@@ -3,6 +3,7 @@ env/bin/python descarga_videos_xmf.py
 env/bin/python descarga_videos_carpintero.py
 env/bin/python descarga_videos_mousikepl.py
 env/bin/python descarga_videos_emilio.py
+env/bin/python descarga_videos_bellonmaceiras.py
 
 git commit -a -m updated
 git push
