5ef210fd642e4f169fadd4ceaa62f294ba6a2e61
lmorillas <morillas@gmail.com>
2018-12-28 08:53:07 +0100

    updated

--- descarga_videos.sh
+++ descarga_videos.sh
@@ -5,7 +5,7 @@ env/bin/python descarga_videos_mousikepl.py
 env/bin/python descarga_videos_emilio.py
 env/bin/python descarga_videos_bellonmaceiras.py
 env/bin/python descarga_videos_coetus.py
-
+env/bin/python improvisor_bs.py
 
 git commit -a -m updated
 git push
