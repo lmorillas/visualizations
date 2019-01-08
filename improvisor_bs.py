f364cb300c12159925def26d6ee1cabeed98bc0a
lmorillas <morillas@gmail.com>
2019-01-08 22:30:24 +0100

    improvisor visual

---
+++ improvisor_bs.py
@@ -0,0 +1,84 @@
+import csv
+import re
+from urllib.parse import urlparse
+import urllib
+import requests
+from bs4 import BeautifulSoup
+import json
+
+'''
+https://www.crummy.com/software/BeautifulSoup/bs4/doc/#kinds-of-filters
+'''
+
+url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQj-2s2x-1_d9Z2z2jvr9VnMo8aiM8vr5lV3U25LXMkwCWYpd3YhyzizrQC-F-LsQ9JZXjuEPNgZk0z/pub'
+
+resp = requests.get(url)
+
+doc = BeautifulSoup(resp.text, 'lxml')
+
+
+datos = []
+for nid, r in enumerate(doc.find_all('tr')):
+    lista = []
+    lista.append(nid)
+    for d in r.find_all('td'):
+        a = d.find_all('a')
+        if a:
+            a = a[0]
+            link = a.attrs['href'].replace('https://www.google.com/url?q=', '')
+            texto = a.text
+            lista.append({'texto':texto, 'link': link})
+        else:
+            lista.append(d.text)
+    datos.append(lista)
+
+
+output = []
+l = [''] * 10
+regex = re.compile(r'(https?://)?(www\.)?(youtube|youtu|youtube-nocookie)\.(com|be)/(watch\?v=|embed/|v/|.+\?v=)?(?P<id>[A-Za-z0-9\-=_]{11})')
+for fila in datos:
+    res = l[:]
+    for n, d in enumerate(fila):
+        if isinstance(d, dict):
+            res[n] = d.get('texto')
+            link = d.get('link')
+            link = urllib.parse.unquote(link)
+            if n == 2:
+                match = regex.match(link)
+                if match:
+                    link = match.group('id')
+                res[-3] = link
+            elif n == 3:
+                res[-2] = res[3]
+                # res[n] = urlparse(res[n]).path.split('/')[-1]
+            else:
+                link = link.split('&')[0]
+                #print(link)
+                res[-1] = link
+        else:
+            res[n] = d
+    output.append(res)
+
+w = csv.writer(open('improvisor.csv', 'w'))
+h = ['id',
+ 'Category',
+ 'label',
+ 'Leadsheet',
+ 'Date',
+ 'Poster',
+ 'Comments',
+ 'title_url',
+ 'ls_url',
+ 'poster_url']
+w.writerow(h)
+w.writerows(output[2:])
+
+lista_dicts = [dict(zip(h, l)) for l in output[2:]]
+datos = [{k: v for k, v in d.items() if v } for d in lista_dicts]    
+
+ruta = 'docs/improVisor/index.html'
+data = json.dumps({'items': datos})
+schema = json.load(open('schema.json'))
+schema = json.dumps(schema)
+template = open('index_improvisor.html').read()
+open(ruta, 'w').write(template.format(data=data, schema=schema))
