254a5005de7de2701b75758616cdedcebcb0ad74
lmorillas <morillas@gmail.com>
2019-03-29 12:22:02 +0100

    typo static

---
+++ docs/js/scripts.min.8798d5bd1f6b83b946619008f3682b64fdaa1408f69228b781042fac9b189884.js
@@ -0,0 +1,3 @@
+var body=document.querySelector('body')
+var menuTrigger=document.querySelector('#toggle-main-menu-mobile');var menuContainer=document.querySelector('#main-menu-mobile');var links=document.querySelectorAll('#main-menu-mobile a');menuTrigger.onclick=function(){menuContainer.classList.toggle('open');menuTrigger.classList.toggle('is-active');body.classList.toggle('lock-scroll');}
+for(i=0;i<links.length;i++){links[i].onclick=function(){menuContainer.classList.toggle('open');menuTrigger.classList.toggle('is-active');body.classList.toggle('lock-scroll');}}
